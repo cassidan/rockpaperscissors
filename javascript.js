e96b1da89707440432e6d2a74991175e42bfa483
Daniel Cassidy <130303841+cassidan@users.noreply.github.com>
2023-04-23 15:17:33 +0100

    Reworked to include GUI

--- javascript.js
+++ javascript.js
@@ -2,23 +2,78 @@
 // array containing options
 const options = ["Rock", "Paper", "Scissors"];
 
+const buttons = document.querySelectorAll('button');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const record = document.querySelector('#record');
+const recordPlayerChoice = document.querySelector('#recordPlayerChoice');
+const recordComputerChoice = document.querySelector('#recordComputerChoice');
+const recordResult = document.querySelector('#recordResult');
+
 // returns random choice from options array
 function getComputerChoice() {
     let computerChoice = options[Math.floor(Math.random()*options.length)];
     return computerChoice;
 }
 
-// gets user input via promp, capitalizes and returns it
-function getPlayerChoice() {
-    let playerInput = prompt("Rock, Paper, or Scissors?");
+function game() {
+  let games = 0;
+  let score = 0;
+  let wins = 0;
+  let losses = 0;
+  let draws = 0;
 
-    let firstLetter = playerInput.charAt(0);
-    let firstLetterCap = firstLetter.toUpperCase();
-    let remainingLetters = playerInput.slice(1);
-    let remainingLettersLow = remainingLetters.toLowerCase();
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      score = playRound(button.id, getComputerChoice());
+      while ((wins < 5) && (losses < 5)) {
+        recordComputerChoice.style.fontSize = "16px";
+        if (score === 2) {
+            wins++;
+            playerScore.textContent = wins;
+            playerScore.style.color = "green";
+            computerScore.textContent = losses;
+            computerScore.style.color = "antiquewhite";
+        } else if (score === 0) {
+            losses++;
+            playerScore.textContent = wins;
+            playerScore.style.color = "antiquewhite";
+            computerScore.textContent = losses;
+            computerScore.style.color = "red";
+        } else {
+            draws++;
+            playerScore.style.color = "antiquewhite";
+            computerScore.style.color = "antiquewhite";
+        }
+        games++;
+        console.log(wins + " wins, " + losses + " losses.");
+        break;
+    }
 
-    let capitalizedWord = firstLetterCap + remainingLettersLow;
-    return capitalizedWord;
+    if ((wins === 5) || (losses === 5)) {
+      if (wins > losses) {
+          recordResult.textContent = "";
+          recordPlayerChoice.textContent = "";
+          recordComputerChoice.style.fontSize = "32px";
+          recordComputerChoice.textContent = "You won the game! Of " + games + " rounds played, you won " + 
+          wins + ", lost " + losses + ", and there were " + draws + " draws.";
+      } else  if (losses > wins) {
+          recordResult.textContent = "";
+          recordPlayerChoice.textContent = "";
+          recordComputerChoice.style.fontSize = "32px";
+          recordComputerChoice.textContent = "You lost the game! Of " + games + " rounds played, you won " + 
+          wins + ", lost " + losses + ", and there were " + draws + " draws.";
+      } else {
+          recordResult.textContent = "";
+          recordPlayerChoice.textContent = "";
+          recordComputerChoice.style.fontSize = "32px";
+          recordComputerChoice.textContent = "You lost the game! Of " + games + " rounds played, you won " + 
+          wins + ", lost " + losses + ", and there were " + draws + " draws.";
+      }
+      games = 0; score = 0; wins = 0; losses = 0; draws = 0;
+    }
+    });
+  });
 }
 
 // play one round, outputs log text as result
@@ -27,59 +82,26 @@ function playRound (playerSelection, computerSelection) {
     let computerChoice = computerSelection;
     let result;
 
-    console.log("You chose: " + playerChoice);
-    console.log("The computer chose: " + computerChoice);
+    recordPlayerChoice.textContent = "You chose: " + playerChoice;
+    recordComputerChoice.textContent = "The computer chose: " + computerChoice;
 
     if ((playerChoice === "Rock") && (computerChoice === "Scissors") || 
         (playerChoice === "Paper") && (computerChoice === "Rock") || 
         (playerChoice === "Scissors") && (computerChoice === "Paper")) {
-          console.log("You win! " + playerChoice + " beats " + computerChoice + "!")
+          recordResult.textContent = "You win! " + playerChoice + " beats " + computerChoice + "!"
           result = 2; // win
           return result;
     } else if 
         ((playerChoice === "Rock") && (computerChoice === "Paper") || 
         (playerChoice === "Paper") && (computerChoice === "Scissors") || 
         (playerChoice === "Scissors") && (computerChoice === "Rock")) {
-          console.log("You lose! " + computerChoice + " beats " + playerChoice + "!")
+          recordResult.textContent = "You lose! " + computerChoice + " beats " + playerChoice + "!"
           result = 0; // loss
           return result;
     } else {
-        console.log("It's a draw!");
+        recordResult.textContent = "It's a draw!";
         result = 1; // draw
     }    
-}
-
-// plays 5 rounds
-function game () {
-    let games = 0;
-    let wins = 0;
-    let losses = 0;
-    let draws = 0;
-    let score = 0;
-
-    while ((games < 5) && (wins < 3) && (losses < 3)) {
-        score = playRound(getPlayerChoice(), getComputerChoice());
-        if (score === 2) {
-            wins++
-        } else if (score === 0) {
-            losses++
-        } else {
-            draws++;
-        }
-        games++;
-        console.log(games + " played, " + wins + " wins, " + losses + " losses.")
-    }
-
-    if (wins > losses) {
-        console.log("You won the game! Of " + games + " rounds played, you won " + 
-        wins + ", lost " + losses + ", and there were " + draws + " draws.")
-    } else  if (losses > wins) {
-        console.log("You lost the game! Of " + games + " rounds played, you won " + 
-        wins + ", lost " + losses + ", and there were " + draws + " draws.")
-    } else {
-        console.log("It's a draw! Of " + games + " rounds played, you won " + 
-        wins + ", lost " + losses + ", and there were " + draws + " draws.")
-    }
 }
 
 game();
